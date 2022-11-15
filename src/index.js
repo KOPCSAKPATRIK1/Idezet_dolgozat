72bd97f0e7e17834bf75969aba121162a6a78be0
Kopcsak Patirk <kopcsakpatrik1@gmail.com>
2022-11-15 09:07:51 +0100

    osszes betoltes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,29 @@ import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Loaded')
+    let eredmeny;
+
+    async function Beolvas()
+    {
+        let response = await fetch('/quotes.json');
+        eredmeny = await response.json();
+    }
+    Beolvas();
+
+    function Betolt(quotesLista){
+        let szulo = document.getElementById("idezetek");
+        quotesLista = quotesLista.sort((a, b) => a.author.localeCompare(b.author));
+
+        for(let e of quotesLista)
+        {
+            let li = document.createElement("li");
+            li.innerHTML = e.author + ": " + e.quote;
+            szulo.appendChild(li);
+        }
+    }
+
+    document.getElementById("betolt").addEventListener("click", ()=>{
+        Betolt(eredmeny.quotes);
+    })
+
 })
